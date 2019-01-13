e69f6a7a34f299266ec32228f705a444ddac5bcc
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2019-01-13 10:57:10 -0800

    add street address

--- lib/geocoder/results/location_iq.rb
+++ lib/geocoder/results/location_iq.rb
@@ -2,5 +2,11 @@ require 'geocoder/results/nominatim'
 
 module Geocoder::Result
   class LocationIq < Nominatim
+    def street_address
+      [
+        house_number,
+        street
+      ].compact.join(" ")
+    end
   end
 end
